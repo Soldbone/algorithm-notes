5abf411a110934f61647f34fe7659146c20e70ca
Soldbone <1472ezs@gmail.com>
2025-09-16 10:22:46 +0900

    [1976 - 여행가자]

--- 1976.py
+++ 1976.py
@@ -1,4 +1,7 @@
 # 여행 가자
+import sys
+input = sys.stdin.readline
+
 
 N = int(input())
 M = int(input())
@@ -11,7 +14,11 @@ a와 b를 합친다.
 int, int -> void
 '''
 def union(a: int, b: int):
-    pass
+    a = parent[a]
+    b = parent[b]
+    if a == b:
+        return
+    parent[b] = a
 
 
 '''
@@ -19,4 +26,23 @@ a의 부모를 찾는다.
 int -> int
 '''
 def find(a: int):
-    pass
+    if a == parent[a]:
+        return a
+    parent[a] = find(parent[a])
+    return parent[a]
+
+for i in range(1, N+1):
+    input_line = list(map(int, input().split()))
+    for j in range(i, N):
+        if input_line[i] == 1:
+            # parent[j+1] = i
+            union(i, j+1)
+
+target_path = list(map(int, input().split()))
+for i in range(N-1):
+    if find(target_path[i]) != find(target_path[i+1]):
+        print("NO")
+        break
+else:
+    print("YES")
+    
